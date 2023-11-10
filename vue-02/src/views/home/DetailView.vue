5ef18dda2a2ac18790d6d005f504acb6611bd31b
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-10 09:26:14 +0800

    Default Changelist

--- vue-02/src/views/home/DetailView.vue
+++ vue-02/src/views/home/DetailView.vue
@@ -12,6 +12,20 @@
       <p style="font-size: 20px;font-weight: bold">￥198
         <span style="font-size: 15px;color: #666">原价:<s>398</s></span>
       </p>
+      <el-row :gutter="20" style="text-align: center">
+        <el-col :span="8">
+          <el-card><img src="/imgs/ewm.jpg"></el-card>
+          <p>扫码关注公众号</p>
+        </el-col>
+        <el-col :span="8">
+          <el-card><img src="/imgs/ewm.jpg"></el-card>
+          <p>扫码购买商品</p>
+        </el-col>
+        <el-col :span="8">
+          <el-card><img src="/imgs/ewm.jpg"></el-card>
+          <p>扫码下载APP</p>
+        </el-col>
+      </el-row>
     </el-col>
   </el-row>
 </template>
