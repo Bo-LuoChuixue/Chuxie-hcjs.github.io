5177d08d4c9e0e742e00cfc1faada3e5a31b7441
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-14 10:33:37 +0800

    Default Changelist

--- baking-web/src/views/AdminView.vue
+++ baking-web/src/views/AdminView.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+ <el-container>
+   <el-header style="background-color: #336688;line-height: 60px" >
+      <h1 style="font-size: 30px;color: white;margin: 0">烘焙坊后台管理系统
+        <span style="float: right;font-size: 20px">欢迎XXX回来!
+          <a href="">退出登录</a>
+        </span>
+      </h1>
+   </el-header>
+   <el-container>
+     <el-aside width="200px">
+        <el-menu router active-text-color="orange" style="padding: 20px;height: 100%">
+          <img src="/imgs/icon.png" style="width: 150px">
+          <el-menu-item index="/admin/user">
+            <el-icon><User/></el-icon>用户管理
+          </el-menu-item>
+          <el-menu-item index="/admin/banner">
+            <el-icon><Picture/></el-icon>轮播图管理
+          </el-menu-item>
+          <el-menu-item index="/admin/content">
+            <el-icon><ChatLineSquare/></el-icon>内容管理
+          </el-menu-item>
+        </el-menu>
+     </el-aside>
+     <el-main>
+       <router-view/>
+     </el-main>
+   </el-container>
+ </el-container>
 </template>
