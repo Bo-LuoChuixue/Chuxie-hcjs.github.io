e7de6f25063ef6407f77051830be56584e8f3198
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-15 14:13:20 +0800

    Default Changelist

--- weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
+++ weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
@@ -1,7 +1,9 @@
 package cn.tedu.weibo.controller;
 
+import cn.tedu.weibo.mapper.UserMapper;
 import cn.tedu.weibo.pojo.dto.UserRegDTO;
 import cn.tedu.weibo.response.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
+    @Autowired
+    UserMapper mapper;
     @PostMapping("reg")
     public JsonResult reg(UserRegDTO userRegDTO){
-        System.out.println("userRegDTO"+userRegDTO);
+        System.out.println("userRegDTO = " + userRegDTO);
+
         return JsonResult.ok();
     }
 }
