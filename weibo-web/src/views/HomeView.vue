817834a92112eefde519b00542d0b6574387e6c2
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-15 10:48:10 +0800

    Default Changelist

--- weibo-web/src/views/HomeView.vue
+++ weibo-web/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>微博首页</h1>
+  <router-link to="/reg">注册</router-link>
+  <router-link to="/login">登录</router-link>
 </template>
 
 <script setup>
---
+++ weibo-web/src/views/RegView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>注册页面</h1>
+  <el-card>
+    <el-form label-width="100px">
+      <el-form-item label="用户名">
+        <el-input v-model="user.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="user.password"></el-input>
+      </el-form-item>
+      <el-form-item label="昵称">
+        <el-input v-model="user.nickname"></el-input>
+      </el-form-item>
+      <el-form-item><el-button>注册</el-button></el-form-item>
+    </el-form>
+  </el-card>
+</template>
+<script setup>
+</script>
