80b6325e50bcc95da8c0ae85ae5759062d19323d
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-15 10:51:57 +0800

    Default Changelist

--- weibo-web/src/views/HomeView.vue
+++ weibo-web/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>微博首页</h1>
+  <div style="display: inline-block;"></div>
   <router-link to="/reg">注册</router-link>
+  <div style="margin-left: 10px; display: inline-block;"></div>
   <router-link to="/login">登录</router-link>
 </template>
 
