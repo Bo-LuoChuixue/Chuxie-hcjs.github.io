0da790b2c4b975cc8e98eb60b8e97410b56cd865
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-16 14:08:17 +0800

    Default Changelist

--- weibo/src/main/java/cn/tedu/weibo/service/impl/WeiboService.java
+++ weibo/src/main/java/cn/tedu/weibo/service/impl/WeiboService.java
@@ -2,15 +2,23 @@ package cn.tedu.weibo.service.impl;
 
 import cn.tedu.weibo.mapper.WeiboMapper;
 import cn.tedu.weibo.pojo.dto.InsertWeiboDTO;
+import cn.tedu.weibo.pojo.entity.Weibo;
 import cn.tedu.weibo.service.IWeiboService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Date;
 
 @Service
 public class WeiboService implements IWeiboService {
     @Autowired
     WeiboMapper mapper;
+
     @Override
     public void addNew(InsertWeiboDTO weiboDTO) {
+        Weibo weibo = new Weibo();
+        BeanUtils.copyProperties(weiboDTO, weibo);
+        weibo.setCreated(new Date());
+        mapper.insert(weibo);
     }
 }
