c5e3dc4caf35a937893b47d5616ae4bd61b31185
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-16 10:37:25 +0800

    Default Changelist

--- weibo-web/src/views/HomeView.vue
+++ weibo-web/src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <template>
   <h1>微博首页</h1>
-  <div style="display: inline-block;"></div>
-  <router-link to="/reg">注册</router-link>
-  <div style="margin-left: 10px; display: inline-block;"></div>
-  <router-link to="/login">登录</router-link>
+  <div v-if="user==null">
+    <div style="display: inline-block;"></div>
+    <router-link to="/reg">注册</router-link>
+    <div style="margin-left: 10px; display: inline-block;"></div>
+    <router-link to="/login">登录</router-link>
+  </div>
+  <div v-else>
+    <h2>欢迎{{user.nickname}}</h2>
+    <a href="">退出登录</a>
+  </div>
 </template>
 
 <script setup>
+import {ref} from "vue";
 
+const user=ref(localStorage.user?JSON.parse(localStorage.user):null);
 </script>
