c25c29cc39ec85e4ccdaf782d6d3a32872f9342c
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-16 09:21:48 +0800

    Default Changelist

--- weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
+++ weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
@@ -1,7 +1,9 @@
 package cn.tedu.weibo.controller;
 
+import cn.tedu.weibo.pojo.dto.UserLoginDTO;
 import cn.tedu.weibo.pojo.dto.UserRegDTO;
 import cn.tedu.weibo.response.JsonResult;
+import cn.tedu.weibo.response.StatusCode;
 import cn.tedu.weibo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,5 +20,10 @@ public class UserController {
     public JsonResult reg(UserRegDTO userRegDTO) {
         service.reg(userRegDTO);
         return JsonResult.ok();
+    }
+    @PostMapping
+    public JsonResult login(UserLoginDTO userLoginDTO){
+        System.out.println("userLoginDTO="+userLoginDTO);
+        return new JsonResult(StatusCode.LOGIN_SUCCESS);
     }
 }
