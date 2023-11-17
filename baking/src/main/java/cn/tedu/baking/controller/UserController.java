4208b889c82c2a7a1a33352ad7ac2df8a9ac4447
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-17 14:26:46 +0800

    Default Changelist

--- baking/src/main/java/cn/tedu/baking/controller/UserController.java
+++ baking/src/main/java/cn/tedu/baking/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.tedu.baking.controller;
 
 import cn.tedu.baking.pojo.dto.UserRegDTO;
 import cn.tedu.baking.response.JsonResult;
+import cn.tedu.baking.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
+    @Autowired
+    IUserService service;
     @PostMapping("reg")
     public JsonResult reg(UserRegDTO userRegDTO){
-        System.out.println("userRegDTO = " + userRegDTO);
+        service.reg(userRegDTO);
         return JsonResult.ok();
     }
 }
