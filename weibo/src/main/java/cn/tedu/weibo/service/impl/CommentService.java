e0dd8b454a0a94d779fda0b372b1828af875e09d
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-17 09:57:59 +0800

    Default Changelist

--- weibo/src/main/java/cn/tedu/weibo/service/impl/CommentService.java
+++ weibo/src/main/java/cn/tedu/weibo/service/impl/CommentService.java
@@ -1,8 +1,24 @@
 package cn.tedu.weibo.service.impl;
 
+import cn.tedu.weibo.mapper.CommentMapper;
+import cn.tedu.weibo.pojo.dto.CommentDTO;
+import cn.tedu.weibo.pojo.entity.Comment;
 import cn.tedu.weibo.service.ICommentService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Date;
 
 @Service
-public class CommentService implements ICommentService{
+public class CommentService implements ICommentService {
+    @Autowired
+    CommentMapper mapper;
+
+    @Override
+    public void addNew(CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        BeanUtils.copyProperties(commentDTO, comment);
+        comment.setCreated(new Date());
+        mapper.insert(comment);
+    }
 }
